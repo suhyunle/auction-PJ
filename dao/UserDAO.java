af858b5e3b815d77d030e3d2f44003d688772291
chQ <gudrb019@naver.com>
2025-04-01 14:55:28 +0900

    d

--- dao/UserDAO.java
+++ dao/UserDAO.java
@@ -1,6 +1,6 @@
 package dao;
 public class UserDAO {
-
+    int dat = 1;
             /*UserDAO.java
         기능: 사용자 관련 DB 연동을 담당. 사용자 정보 삽입, 조회, 수정, 삭제 등의 CRUD 작업을 처리.
 
@@ -11,5 +11,5 @@ public class UserDAO {
         사용자 등록
 
         사용자 수정 및 삭제 */
-    
+
 }
